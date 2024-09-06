6656d573a185d1c4c28dd5f761bfd965a5bc8e55
Shuhrat <shkarimov@phoenixit.ru>
2024-09-06 16:20:28 +0300

    add tree js

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -1,4 +1,4 @@
-import throttle from 'lodash/throttle';
+import throttle from "lodash/throttle";
 
 export default class FullPageScroll {
   constructor() {
@@ -6,8 +6,12 @@ export default class FullPageScroll {
     this.scrollFlag = true;
     this.timeout = null;
 
-    this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
-    this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
+    this.screenElements = document.querySelectorAll(
+      `.screen:not(.screen--result)`
+    );
+    this.menuElements = document.querySelectorAll(
+      `.page-header__menu .js-menu-link`
+    );
 
     this.activeScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
@@ -15,9 +19,11 @@ export default class FullPageScroll {
   }
 
   init() {
-    document.addEventListener(`wheel`, throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, {trailing: true}));
+    document.addEventListener(
+      `wheel`,
+      throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, { trailing: true })
+    );
     window.addEventListener(`popstate`, this.onUrlHashChengedHandler);
-
     this.onUrlHashChanged();
   }
 
@@ -40,8 +46,10 @@ export default class FullPageScroll {
   }
 
   onUrlHashChanged() {
-    const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
-    this.activeScreen = (newIndex < 0) ? 0 : newIndex;
+    const newIndex = Array.from(this.screenElements).findIndex(
+      (screen) => location.hash.slice(1) === screen.id
+    );
+    this.activeScreen = newIndex < 0 ? 0 : newIndex;
     this.changePageDisplay();
   }
 
@@ -63,7 +71,9 @@ export default class FullPageScroll {
   }
 
   changeActiveMenuItem() {
-    const activeItem = Array.from(this.menuElements).find((item) => item.dataset.href === this.screenElements[this.activeScreen].id);
+    const activeItem = Array.from(this.menuElements).find(
+      (item) => item.dataset.href === this.screenElements[this.activeScreen].id
+    );
     if (activeItem) {
       this.menuElements.forEach((item) => item.classList.remove(`active`));
       activeItem.classList.add(`active`);
@@ -73,10 +83,10 @@ export default class FullPageScroll {
   emitChangeDisplayEvent() {
     const event = new CustomEvent(`screenChanged`, {
       detail: {
-        'screenId': this.activeScreen,
-        'screenName': this.screenElements[this.activeScreen].id,
-        'screenElement': this.screenElements[this.activeScreen]
-      }
+        screenId: this.activeScreen,
+        screenName: this.screenElements[this.activeScreen].id,
+        screenElement: this.screenElements[this.activeScreen],
+      },
     });
 
     document.body.dispatchEvent(event);
@@ -84,7 +94,10 @@ export default class FullPageScroll {
 
   reCalculateActiveScreenPosition(delta) {
     if (delta > 0) {
-      this.activeScreen = Math.min(this.screenElements.length - 1, ++this.activeScreen);
+      this.activeScreen = Math.min(
+        this.screenElements.length - 1,
+        ++this.activeScreen
+      );
     } else {
       this.activeScreen = Math.max(0, --this.activeScreen);
     }
--- source/js/modules/tree.js
+++ source/js/modules/tree.js
@@ -1,9 +1,12 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import path from "path";
 /**
  * дока https://threejs.org/manual/#ru/fundamentals
  */
 
 const canvas = document.querySelector("#treejs");
+
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
 const fov = 75; // поле зрения.
@@ -11,47 +14,101 @@ const fov = 75; // поле зрения.
 const aspect = window.innerWidth / window.innerHeight; // значение для canvas по умолчанию
 const near = 0.1; //Усеченная форма камеры вблизи плоскости
 //Представляют пространство перед камерой, которое будет отображаться. Все, что находится до или после этого диапазона, будет обрезано (не нарисовано).
-const far = 2000; //Дальняя плоскость усеченной камеры. Значение по умолчанию - 2000.
+const far = 1000; //Дальняя плоскость усеченной камеры. Значение по умолчанию - 2000.
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-camera.position.z = 4;
-camera.position.y = 1;
+camera.position.z = 0.6;
+camera.position.y = 0;
 // camera.rotateY((45 * Math.PI) / 180);
-// camera.rotateX((45 * Math.PI) / 180);
+// camera.rotateX((rotateX * Math.PI) / 180);
 // camera.rotateZ((45 * Math.PI) / 180);
 
 const scene = new THREE.Scene();
 
-const boxWidth = 1;
+const boxWidth = 2;
 const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
+const geometry = new THREE.PlaneGeometry(boxWidth, boxHeight);
 
-const cube = new THREE.Mesh(geometry, material); //Полигональная сетка
-// scene.add(cube);
+const loader = new THREE.TextureLoader();
+const textures = Array(5)
+  .fill(null)
+  .map((_, index) => {
+    const texture = loader.load(
+      path.resolve(__dirname, `img/module-5/scenes-textures/scene-${index}.png`)
+    );
+
+    return texture;
+  });
+
+const material1 = new THREE.MeshBasicMaterial({
+  map: textures.at(0),
+  side: THREE.DoubleSide,
+});
+const material2 = new THREE.MeshBasicMaterial({
+  map: textures.at(1),
+  side: THREE.DoubleSide,
+});
+const material3 = new THREE.MeshBasicMaterial({
+  map: textures.at(2),
+  side: THREE.DoubleSide,
+});
+const material4 = new THREE.MeshBasicMaterial({
+  map: textures.at(3),
+  side: THREE.DoubleSide,
+});
+
+const material5 = new THREE.MeshBasicMaterial({
+  map: textures.at(4),
+  side: THREE.DoubleSide,
+});
+
+const plane1 = new THREE.Mesh(geometry, material1); //Полигональная сетка фигуры
+const plane2 = new THREE.Mesh(geometry, material2); //Полигональная сетка фигуры
+const plane3 = new THREE.Mesh(geometry, material3); //Полигональная сетка фигуры
+const plane4 = new THREE.Mesh(geometry, material4); //Полигональная сетка фигуры
+const plane5 = new THREE.Mesh(geometry, material5); //Полигональная сетка фигуры
+
+plane2.translateX(plane1.position.x + plane1.geometry.parameters.width);
+plane3.translateX(plane2.position.x + plane2.geometry.parameters.width);
+plane4.translateX(plane3.position.x + plane3.geometry.parameters.width);
+plane5.translateX(plane4.position.x + plane4.geometry.parameters.width);
+
+scene.add(plane1);
+scene.add(plane2);
+scene.add(plane3);
+scene.add(plane4);
+scene.add(plane5);
+
+// camera.position.x = 4;
+// const controls = new TrackballControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement); // Управление орбитой позволяет камере вращаться вокруг цели.
 
 /** HELPERS  */
 const cameraHelper = new THREE.CameraHelper(camera); // оси камеры
 const arrowHelper = new THREE.ArrowHelper(); // cвет
-const boxHelper = new THREE.BoxHelper(cube); // граница фигуры
+// const boxHelper = new THREE.PlaneHelper(plane); // граница фигуры
 const grid = new THREE.GridHelper(5, 25); // отражение горизонта
 const axesHelper = new THREE.AxesHelper(5); // оси координат
-scene.add(axesHelper, grid, cameraHelper, arrowHelper, boxHelper);
-
+scene.add(axesHelper, grid, cameraHelper, arrowHelper);
 /** END HELPERS  */
 
+function render() {
+  renderer.render(scene, camera);
+}
+
+document.body.addEventListener("screenChanged", ({ detail }) => {
+  console.log(detail);
+});
+
+// Функция рендера (зациклена)
+(function animate() {
+  console.log("animate");
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  render();
+})();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
-
-// function render(time) {
-//   time *= 0.0007; // конвертировать время в секунды
-
-//   cube.rotation.x = time;
-//   cube.rotation.y = time;
-
-//   renderer.render(scene, camera);
-
-//   requestAnimationFrame(render);
-// }
-// requestAnimationFrame(render);
+render();
